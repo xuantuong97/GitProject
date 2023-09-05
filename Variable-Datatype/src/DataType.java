5f9d1af06c78e2387e7ab1a0ea9620ed256f1b11
xuantuong97 <tieutho2026@gmail.com>
2023-09-05 22:17:11 +0700

    frompc

---
+++ Variable-Datatype/src/DataType.java
@@ -0,0 +1,13 @@
+public class DataType {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "code";
+        System.out.println("i = " + i);
+    }
+}
+
+
