f936f7b2a98bea059877dccf587f2946752b6c3c
xuantuong97 <tieutho2026@gmail.com>
2023-09-14 15:20:50 +0700

    h

---
+++ Module_2/Introduction/src/module_2/refactor/TennisGame.java
@@ -0,0 +1,100 @@
+package module_2.refactor;
+
+public class TennisGame {
+    public static final String DASH_CHAR = "-";
+    public static final String LOVE = "Love";
+    public static final String FIFTEEN = "Fifteen";
+    public static final String THIRTY = "Thirty";
+    public static final String FORTY = "Forty";
+    static String score = "";
+    static int tempScore = 0;
+
+    public static String getScore( int player1Score, int player2Score) {
+
+        boolean isScoreEqual = player1Score == player2Score;
+        boolean isScoreGreaterThan4 = player1Score >=4 || player2Score >=4;
+
+        if (isScoreEqual)
+        {
+            equalScore(player1Score);
+        }
+        else if (isScoreGreaterThan4)
+        {
+            ScoreGreaterThan4(player1Score, player2Score);
+        }
+        else
+        {
+            otherScore(player1Score, player2Score);
+        }
+        return score;
+    }
+
+    static void equalScore( int playerScore){
+        switch (playerScore)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+    }
+
+    static void ScoreGreaterThan4(int player1Score, int player2Score){
+        int minusResult = player1Score - player2Score;
+
+        if (minusResult == 1) {
+            score = "Advantage player 1";
+        }
+        else if (minusResult == -1) {
+            score = "Advantage player 2";
+        }
+        else if (minusResult >=2 ) {
+            score = "Win for player 1";
+        }
+        else {
+            score = "Win for player 2";
+        }
+    }
+
+    static void otherScore (int player1Score, int player2Score){
+        for (int i=1; i<3; i++)
+        {
+            if (i == 1) {
+                tempScore = player1Score;
+            }
+            else {
+                score += DASH_CHAR;
+                tempScore = player2Score;
+            }
+
+            switch(tempScore)
+            {
+                case 0:
+                    score += LOVE;
+                    break;
+                case 1:
+                    score += FIFTEEN;
+                    break;
+                case 2:
+                    score += THIRTY;
+                    break;
+                case 3:
+                    score += FORTY;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
